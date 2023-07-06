23fde646651b8e0cc1740f6939fba2f34f224cd5
Raiki Tamura <tamaron1203@gmail.com>
2023-07-06 17:10:51 +0900

    SDL2を削除, windows用のビルド方法を追加

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # n64-emu
 
 # Build
+## Windows
+1.Boostを[公式サイト](https://www.boost.org/users/download/)からダウンロードし、ビルドする
+2. `BOOST_ROOR`, `BOOST_INCLUDEDIR`, `BOOST_LIBRARYDIR`を以下のように設定する。
+
+```
+BOOST_INCLUDEDIR    path/to/boost/include
+BOOST_LIBRARYDIR    path/to/boost/lib
+BOOST_ROOT          path/to/boost
+```
+
+3. 以下のコマンドを実行
+```
+git clone git@github.com:kmc-jp/n64-emu.git
+cd n64-emu
+mkdir build
+cd build
+cmake ..
+make # or cmake --build .
+```
+
+## Linux(Ubuntu)
 ```
 # install boost
 sudo apt install libboost-all-dev libsdl2-2.0-0 libsdl2-dev
@@ -10,5 +31,5 @@ cd n64-emu
 mkdir build
 cd build
 cmake ..
-make
+make # or cmake --build .
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,7 @@
-#include <SDL2/SDL.h>
 #include <boost/format.hpp>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
   std::cout << boost::format("%2% %1%") % 3 % std::string("Hello") << std::endl;
-
-  SDL_Init(SDL_INIT_VIDEO); // Initialize SDL2
-
-  SDL_Window *window; // Declare a pointer to an SDL_Window
-
-  // Create an application window with the following settings:
-  window = SDL_CreateWindow(
-      "An SDL2 window",        //    const char* title
-      SDL_WINDOWPOS_UNDEFINED, //    int x: initial x position
-      SDL_WINDOWPOS_UNDEFINED, //    int y: initial y position
-      640,                     //    int w: width, in pixels
-      480,                     //    int h: height, in pixels
-      SDL_WINDOW_SHOWN         //    Uint32 flags: window options, see docs
-  );
-
-  // Check that the window was successfully made
-  if (window == NULL) {
-    // In the event that the window could not be made...
-    std::cout << "Could not create window: " << SDL_GetError() << '\n';
-    SDL_Quit();
-    return 1;
-  }
-
-  // The window is open: enter program loop (see SDL_PollEvent)
-  SDL_Delay(3000); // Wait for 3000 milliseconds, for example
-
-  // Close and destroy the window
-  SDL_DestroyWindow(window);
-
-  // Clean up SDL2 and exit the program
-  SDL_Quit();
   return 0;
 }
