317b50e89e3a4635d40e6ce6ebe1e79a82047096
Raiki Tamura <tamaron1203@gmail.com>
2023-07-08 18:38:40 +0900

    chore

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -4,14 +4,17 @@
 namespace N64 {
 
 void Cpu::step() {
-    // fetch
+    // instruction fetch
     uint32_t paddr_of_pc = Mmu::resolve_vaddr(pc);
-
-    spdlog::debug("vaddr 0x{:x} -> paddr 0x{:x}", pc, paddr_of_pc);
+    // spdlog::debug("vaddr 0x{:x} -> paddr 0x{:x}", pc, paddr_of_pc);
 
     uint32_t raw_inst = Bus::read_paddr32(paddr_of_pc);
     spdlog::debug("fetch inst = 0x{:x} from paddr = 0x{:x}", raw_inst,
                   paddr_of_pc);
+    
+    // TODO: decode stage
+
+    // TODO: execute, memory, write-back stage
 
     spdlog::critical("Unimplemented. abort");
     N64::n64cpu.dump();
