31bd4a7ff3fab94f03c8605af4679720de9cff0d
sashi0034 <satoshi3104.nishi@gmail.com>
2023-07-12 15:33:23 +0900

    romをヘッダとソースに分離

--- src/memory/rom.h
+++ src/memory/rom.h
@@ -44,31 +44,7 @@ class Rom {
   public:
     Rom() : rom({}), broken(true) {}
 
-    void read_from(std::string filepath) {
-        spdlog::debug("reading from ROM");
-
-        std::ifstream file(filepath.c_str(), std::ios::in | std::ios::binary);
-        if (!file.is_open()) {
-            spdlog::error("Could not open ROM file");
-            return;
-        }
-        rom = {std::istreambuf_iterator<char>(file),
-               std::istreambuf_iterator<char>()};
-        // size of phisical memory region mapped to memory
-        rom.resize(0xFC00000);
-
-        if (rom.size() < sizeof(rom_header_t)) {
-            spdlog::error("ROM is too small");
-            return;
-        }
-
-        // set header
-        header = *((rom_header_t *)rom.data());
-
-        spdlog::debug("ROM size\t= {}", rom.size());
-        spdlog::debug("imageName\t= \"{}\"", std::string(header.image_name));
-        broken = false;
-    }
+    void read_from(const std::string &filepath);
 
     // bool is_broken() const { return broken; }
 
