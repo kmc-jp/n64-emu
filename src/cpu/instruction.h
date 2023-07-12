4237b8bd54b7db7d4ca6ef5e613a294fad4441e4
sashi0034 <satoshi3104.nishi@gmail.com>
2023-07-12 11:09:41 +0900

    命令の定数をconstexpr

--- src/cpu/instruction.h
+++ src/cpu/instruction.h
@@ -112,27 +112,27 @@ class Instruction {
 */
 
 // MIPS instructions
-const uint8_t OPCODE_SPECIAL = 0b000000;
-const uint8_t OPCODE_COP0 = 0b010000;
-const uint8_t OPCODE_ADDIU = 0b001001;
-const uint8_t OPCODE_LW = 0b100011;
-const uint8_t OPCODE_SW = 0b101011;
-const uint8_t OPCODE_LUI = 0b001111;
-const uint8_t OPCODE_BNE = 0b000101; // 2cycles?
-const uint8_t OPCODE_CACHE = 0b101111;
-const uint8_t OPCODE_ANDI = 0b001100;
-const uint8_t OPCODE_ORI = 0b001101;
-const uint8_t OPCODE_XORI = 0b001110;
+constexpr uint8_t OPCODE_SPECIAL = 0b000000;
+constexpr uint8_t OPCODE_COP0 = 0b010000;
+constexpr uint8_t OPCODE_ADDIU = 0b001001;
+constexpr uint8_t OPCODE_LW = 0b100011;
+constexpr uint8_t OPCODE_SW = 0b101011;
+constexpr uint8_t OPCODE_LUI = 0b001111;
+constexpr uint8_t OPCODE_BNE = 0b000101; // 2cycles?
+constexpr uint8_t OPCODE_CACHE = 0b101111;
+constexpr uint8_t OPCODE_ANDI = 0b001100;
+constexpr uint8_t OPCODE_ORI = 0b001101;
+constexpr uint8_t OPCODE_XORI = 0b001110;
 
-const uint8_t SPECIAL_FUNCT_SLL = 0b000000;  // SLL
-const uint8_t SPECIAL_FUNCT_SLTU = 0b101011; // SLTU
-const uint8_t SPECIAL_FUNCT_AND = 0b100100;  // AND
-const uint8_t SPECIAL_FUNCT_OR = 0b100101;   // OR
-const uint8_t SPECIAL_FUNCT_XOR = 0b100110;  // XOR
-const uint8_t SPECIAL_FUNCT_JR = 0b001000;   // JR
+constexpr uint8_t SPECIAL_FUNCT_SLL = 0b000000;  // SLL
+constexpr uint8_t SPECIAL_FUNCT_SLTU = 0b101011; // SLTU
+constexpr uint8_t SPECIAL_FUNCT_AND = 0b100100;  // AND
+constexpr uint8_t SPECIAL_FUNCT_OR = 0b100101;   // OR
+constexpr uint8_t SPECIAL_FUNCT_XOR = 0b100110;  // XOR
+constexpr uint8_t SPECIAL_FUNCT_JR = 0b001000;   // JR
 
-const uint8_t CP0_SUB_MF = 0b00000; // MFC0
-const uint8_t CP0_SUB_MT = 0b00100; // MTC0
+constexpr uint8_t CP0_SUB_MF = 0b00000; // MFC0
+constexpr uint8_t CP0_SUB_MT = 0b00100; // MTC0
 
 } // namespace Cpu
 } // namespace N64
