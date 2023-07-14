07c37b40b4eb69c5f922a677614c9db223a7fbc2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-07-14 21:57:28 +0900

    Add customization for offset

--- src/utils/enum_view.h
+++ src/utils/enum_view.h
@@ -14,17 +14,24 @@
 
 namespace Utils {
 class EnumView {
+    using size_type = std::string_view::size_type;
     // TODO: add magic numbers for other compilers
 #if defined(__clang__)
-    static constexpr auto offset = 38;
+    static constexpr size_type prefix_offset = 38;
+    static constexpr size_type suffix_offset = 1;
+#elif defined(_MSC_VER)
+    static constexpr size_type prefix_offset = 0;
+    static constexpr size_type suffix_offset = 0;
 #else
-    static constexpr auto offset = 0;
+    static constexpr size_type prefix_offset = 0;
+    static constexpr size_type suffix_offset = 0;
 #endif
     template <bool B> using enabler_t = std::enable_if_t<B, std::nullptr_t>;
     template <auto E, enabler_t<std::is_enum_v<decltype(E)>> = nullptr>
     static constexpr auto n() {
         constexpr std::string_view name{PRETTY_FUNCTION};
-        return name.substr(offset, name.size() - offset - 1);
+        return name.substr(prefix_offset,
+                           name.size() - prefix_offset - suffix_offset);
     }
 
   public:
