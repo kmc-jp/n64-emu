221b56d1e31e45acfe40c5455458beea92ea2079
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-07-14 21:52:05 +0900

    Add ifdef branch for MSVC

--- src/utils/enum_view.h
+++ src/utils/enum_view.h
@@ -4,6 +4,14 @@
 #include <string_view>
 #include <type_traits>
 
+#if defined(__clang__)
+#define PRETTY_FUNCTION __PRETTY_FUNCTION__
+#elif defined(_MSC_VER)
+#define PRETTY_FUNCTION __FUNCSIG__
+#else
+#define PRETTY_FUNCTION __func__
+#endif
+
 namespace Utils {
 class EnumView {
     // TODO: add magic numbers for other compilers
@@ -15,7 +23,7 @@ class EnumView {
     template <bool B> using enabler_t = std::enable_if_t<B, std::nullptr_t>;
     template <auto E, enabler_t<std::is_enum_v<decltype(E)>> = nullptr>
     static constexpr auto n() {
-        constexpr std::string_view name{__PRETTY_FUNCTION__};
+        constexpr std::string_view name{PRETTY_FUNCTION};
         return name.substr(offset, name.size() - offset - 1);
     }
 
