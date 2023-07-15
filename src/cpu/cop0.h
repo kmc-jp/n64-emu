ec2deaf5ce71cb0177e6ec6fca69165d5e7ae7ca
sashi0034 <satoshi3104.nishi@gmail.com>
2023-07-15 21:06:28 +0900

    ヘッダを軽く

--- src/cpu/cop0.h
+++ src/cpu/cop0.h
@@ -142,56 +142,18 @@ class Cop0 {
     // 31st regiser is unknwon
     */
 
-    Cop0() {}
+    Cop0() = default;
 
-    void reset() {
-        spdlog::debug("initializing CPU COP0");
-        // https://github.com/SimoneN64/Kaizen/blob/dffd36fc31731a0391a9b90f88ac2e5ed5d3f9ec/src/backend/core/registers/Cop0.cpp#L11
-        reg[Cop0Reg::CAUSE] = 0xB000007C;
-        reg[Cop0Reg::STATUS] = 0;
-        get_status_ref()->cu0 = 1;
-        get_status_ref()->cu1 = 1;
-        get_status_ref()->fr = 1;
-        reg[Cop0Reg::PRID] = 0x00000B22;
-        reg[Cop0Reg::CONFIG] = 0x7006E463;
-        reg[Cop0Reg::EPC] = 0xFFFFFFFFFFFFFFFFll;
-        reg[Cop0Reg::ERROR_EPC] = 0xFFFFFFFFFFFFFFFFll;
-        reg[Cop0Reg::WIRED] = 0;
-        reg[Cop0Reg::INDEX] = 63;
-        reg[Cop0Reg::BAD_VADDR] = 0xFFFFFFFFFFFFFFFF;
-    }
+    void reset();
 
-    void dump() {
-        for (int i = 0; i < 16; i++) {
-            spdlog::info("CP0[{}]\t= {:#018x}\tCP0[{}]\t= {:#018x}", i, reg[i],
-                         i + 16, reg[i + 16]);
-        }
-        cop0_status_t *s = get_status_ref();
-        spdlog::info("global interrupt enabled; ie\t= {}",
-                     s->ie ? "enabled" : "disabled");
-        spdlog::info("exception level; exl\t= {:d}", (uint32_t)s->exl);
-        spdlog::info("error level; erl\t= {:d}", (uint32_t)s->erl);
-        spdlog::info("execution mode; ksu\t= {:d}", (uint32_t)s->ksu);
-        spdlog::info("64bit addressing in user mode; ux\t= {}",
-                     s->ux ? "yes" : "no");
-        spdlog::info("64bit addressing in supervisor mode; sx\t= {}",
-                     s->sx ? "yes" : "no");
-        spdlog::info("64bit addressing in kernel mode; kx\t= {}",
-                     s->kx ? "yes" : "no");
-        spdlog::info("interrupt mask; im\t= {:#010b}", (uint32_t)s->im);
-        // TODO: add more
-        spdlog::info("cu0\t= {}\tcu2\t= {}", s->cu0 ? "enabled" : "disabled",
-                     s->cu2 ? "enabled" : "disabled");
-        spdlog::info("cu1\t= {}\tcu3\t= {}", s->cu1 ? "enabled" : "disabled",
-                     s->cu3 ? "enabled" : "disabled");
-    }
+    void dump();
 
     cop0_cause_t *get_cause_ref() {
-        return (cop0_cause_t *)&reg[Cop0Reg::CAUSE];
+        return reinterpret_cast<cop0_cause_t *>(&reg[Cop0Reg::CAUSE]);
     }
 
     cop0_status_t *get_status_ref() {
-        return (cop0_status_t *)&reg[Cop0Reg::STATUS];
+        return reinterpret_cast<cop0_status_t *>(&reg[Cop0Reg::STATUS]);
     }
 
     uint8_t get_interrupt_pending_masked() {
