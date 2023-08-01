93bf2886c4cdd80a06fd1753f6321cea6b29dd26
Raiki Tamura <tamaron1203@gmail.com>
2023-08-01 14:32:14 +0900

    ok

--- src/mmio/mi.cpp
+++ src/mmio/mi.cpp
@@ -16,11 +16,12 @@ uint32_t MI::read_paddr32(uint32_t paddr) const {
     case PADDR_MODE:
         return reg_mode;
     case PADDR_VERSION:
+        Utils::abort("Correct? Read from MI version");
         return reg_version;
     case PADDR_INTERRUPT:
         return reg_interrupt;
     case PADDR_MASK: {
-        Utils::unimplemented("Read from MI MASK");
+        return reg_mask.raw;
     } break;
     default: {
         Utils::critical("Unimplemented. Read from MI paddr = {:#010x}", paddr);
@@ -36,7 +37,42 @@ void MI::write_paddr32(uint32_t paddr, uint32_t value) {
         reg_version = value;
     } break;
     case PADDR_MASK: {
-        Utils::unimplemented("Write to MI MASK");
+        if (value & 1)
+            // clear sp interrupt mask
+            reg_mask.sp = 0;
+        if (value & 2)
+            // set sp interrupt mask
+            reg_mask.sp = 1;
+        if (value & 4)
+            // clear si interrupt mask
+            reg_mask.si = 0;
+        if (value & 8)
+            // set si interrupt mask
+            reg_mask.si = 1;
+        if (value & 16)
+            // clear ai interrupt mask
+            reg_mask.ai = 0;
+        if (value & 32)
+            // set ai interrupt mask
+            reg_mask.ai = 1;
+        if (value & 64)
+            // clear vi interrupt mask
+            reg_mask.vi = 0;
+        if (value & 128)
+            // set vi interrupt mask
+            reg_mask.vi = 1;
+        if (value & 256)
+            // clear pi interrupt mask
+            reg_mask.pi = 0;
+        if (value & 512)
+            // set pi interrupt mask
+            reg_mask.pi = 1;
+        if (value & 1024)
+            // clear dp interrupt mask
+            reg_mask.dp = 0;
+        if (value & 2048)
+            // set dp interrupt mask
+            reg_mask.dp = 1;
     } break;
     default: {
         Utils::critical("Unimplemented. Write to MI paddr = {:#010x}", paddr);
--- src/mmio/mi.h
+++ src/mmio/mi.h
@@ -2,6 +2,7 @@
 #define MI_H
 
 #include "memory_map.h"
+#include "utils.h"
 #include <array>
 #include <cstdint>
 
@@ -15,9 +16,10 @@ const uint32_t PADDR_INTERRUPT = 0x04300008;
 const uint32_t PADDR_MASK = 0x0430000c;
 
 // https://github.com/Dillonb/n64/blob/6502f7d2f163c3f14da5bff8cd6d5ccc47143156/src/system/n64system.h#L64
+// TODO: big endian?
 typedef union mi_intr {
     uint32_t raw;
-    struct {
+    PACK(struct {
         unsigned sp : 1;
         unsigned si : 1;
         unsigned ai : 1;
@@ -25,10 +27,28 @@ typedef union mi_intr {
         unsigned pi : 1;
         unsigned dp : 1;
         unsigned : 26;
-    };
+    });
 } mi_intr_t;
 
 static_assert(sizeof(mi_intr_t) == 4, "mi_intr_t size is not 4 bytes");
+
+// https://github.com/Dillonb/n64/blob/6502f7d2f163c3f14da5bff8cd6d5ccc47143156/src/system/n64system.h#L51
+// TODO: big endian?
+typedef union mi_intr_mask {
+    uint32_t raw;
+    PACK(struct {
+        unsigned sp : 1;
+        unsigned si : 1;
+        unsigned ai : 1;
+        unsigned vi : 1;
+        unsigned pi : 1;
+        unsigned dp : 1;
+        unsigned : 26;
+    });
+} mi_intr_mask_t;
+
+static_assert(sizeof(mi_intr_mask_t) == 4,
+              "mi_intr_mask_t size is not 4 bytes");
 
 // MIPS Interface
 // https://n64brew.dev/wiki/MIPS_Interface
@@ -37,7 +57,7 @@ class MI {
     uint32_t reg_mode;
     uint32_t reg_version;
     uint32_t reg_interrupt;
-    uint32_t reg_mask;
+    mi_intr_mask_t reg_mask;
 
     static MI instance;
 
