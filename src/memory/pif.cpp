1bf3deee33a8cdece5973acac46490965435da81
Raiki Tamura <tamaron1203@gmail.com>
2023-08-04 12:23:18 +0900

    clean up pif.cpp

--- src/memory/pif.cpp
+++ src/memory/pif.cpp
@@ -22,20 +22,20 @@ void pif_hle_execute() {
     case CicType::CIC_NUS_6101: {
         g_cpu().gpr.write(0, 0x0000000000000000);
         g_cpu().gpr.write(1, 0x0000000000000000);
-        g_cpu().gpr.write(2, 0xFFFFFFFFDF6445CCll);
-        g_cpu().gpr.write(3, 0xFFFFFFFFDF6445CCll);
+        g_cpu().gpr.write(2, 0xFFFFFFFFDF6445CC);
+        g_cpu().gpr.write(3, 0xFFFFFFFFDF6445CC);
         g_cpu().gpr.write(4, 0x00000000000045CC);
         g_cpu().gpr.write(5, 0x0000000073EE317A);
-        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0Cll);
-        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08ll);
+        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0C);
+        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08);
         g_cpu().gpr.write(8, 0x00000000000000C0);
         g_cpu().gpr.write(9, 0x0000000000000000);
         g_cpu().gpr.write(10, 0x0000000000000040);
-        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040ll);
-        g_cpu().gpr.write(12, 0xFFFFFFFFC7601FACll);
-        g_cpu().gpr.write(13, 0xFFFFFFFFC7601FACll);
-        g_cpu().gpr.write(14, 0xFFFFFFFFB48E2ED6ll);
-        g_cpu().gpr.write(15, 0xFFFFFFFFBA1A7D4Bll);
+        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040);
+        g_cpu().gpr.write(12, 0xFFFFFFFFC7601FAC);
+        g_cpu().gpr.write(13, 0xFFFFFFFFC7601FAC);
+        g_cpu().gpr.write(14, 0xFFFFFFFFB48E2ED6);
+        g_cpu().gpr.write(15, 0xFFFFFFFFBA1A7D4B);
         g_cpu().gpr.write(16, 0x0000000000000000);
         g_cpu().gpr.write(17, 0x0000000000000000);
         g_cpu().gpr.write(18, 0x0000000000000000);
@@ -44,16 +44,16 @@ void pif_hle_execute() {
         g_cpu().gpr.write(21, 0x0000000000000000);
         g_cpu().gpr.write(23, 0x0000000000000001);
         g_cpu().gpr.write(24, 0x0000000000000002);
-        g_cpu().gpr.write(25, 0xFFFFFFFF905F4718ll);
+        g_cpu().gpr.write(25, 0xFFFFFFFF905F4718);
         g_cpu().gpr.write(26, 0x0000000000000000);
         g_cpu().gpr.write(27, 0x0000000000000000);
         g_cpu().gpr.write(28, 0x0000000000000000);
-        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0ll);
+        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0);
         g_cpu().gpr.write(30, 0x0000000000000000);
-        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550ll);
+        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550);
 
-        g_cpu().lo = 0xFFFFFFFFBA1A7D4Bll;
-        g_cpu().hi = 0xFFFFFFFF997EC317ll;
+        g_cpu().lo = 0xFFFFFFFFBA1A7D4B;
+        g_cpu().hi = 0xFFFFFFFF997EC317;
     } break;
     case CicType::CIC_NUS_7102: {
         g_cpu().gpr.write(0, 0x0000000000000000);
@@ -62,16 +62,16 @@ void pif_hle_execute() {
         g_cpu().gpr.write(3, 0x000000001E324416);
         g_cpu().gpr.write(4, 0x0000000000004416);
         g_cpu().gpr.write(5, 0x000000000EC5D9AF);
-        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0Cll);
-        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08ll);
+        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0C);
+        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08);
         g_cpu().gpr.write(8, 0x00000000000000C0);
         g_cpu().gpr.write(9, 0x0000000000000000);
         g_cpu().gpr.write(10, 0x0000000000000040);
-        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040ll);
+        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040);
         g_cpu().gpr.write(12, 0x00000000495D3D7B);
-        g_cpu().gpr.write(13, 0xFFFFFFFF8B3DFA1Ell);
+        g_cpu().gpr.write(13, 0xFFFFFFFF8B3DFA1E);
         g_cpu().gpr.write(14, 0x000000004798E4D4);
-        g_cpu().gpr.write(15, 0xFFFFFFFFF1D30682ll);
+        g_cpu().gpr.write(15, 0xFFFFFFFFF1D30682);
         g_cpu().gpr.write(16, 0x0000000000000000);
         g_cpu().gpr.write(17, 0x0000000000000000);
         g_cpu().gpr.write(18, 0x0000000000000000);
@@ -85,11 +85,11 @@ void pif_hle_execute() {
         g_cpu().gpr.write(26, 0x0000000000000000);
         g_cpu().gpr.write(27, 0x0000000000000000);
         g_cpu().gpr.write(28, 0x0000000000000000);
-        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0ll);
+        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0);
         g_cpu().gpr.write(30, 0x0000000000000000);
-        g_cpu().gpr.write(31, 0xFFFFFFFFA4001554ll);
+        g_cpu().gpr.write(31, 0xFFFFFFFFA4001554);
 
-        g_cpu().lo = 0xFFFFFFFFF1D30682ll;
+        g_cpu().lo = 0xFFFFFFFFF1D30682;
         g_cpu().hi = 0x0000000010054A98;
     } break;
     case CicType::CIC_NUS_6102_7101: {
@@ -98,14 +98,14 @@ void pif_hle_execute() {
         g_cpu().gpr.write(2, 0x000000000EBDA536);
         g_cpu().gpr.write(3, 0x000000000EBDA536);
         g_cpu().gpr.write(4, 0x000000000000A536);
-        g_cpu().gpr.write(5, 0xFFFFFFFFC0F1D859ll);
-        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0Cll);
-        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08ll);
+        g_cpu().gpr.write(5, 0xFFFFFFFFC0F1D859);
+        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0C);
+        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08);
         g_cpu().gpr.write(8, 0x00000000000000C0);
         g_cpu().gpr.write(9, 0x0000000000000000);
         g_cpu().gpr.write(10, 0x0000000000000040);
-        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040ll);
-        g_cpu().gpr.write(12, 0xFFFFFFFFED10D0B3ll);
+        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040);
+        g_cpu().gpr.write(12, 0xFFFFFFFFED10D0B3);
         g_cpu().gpr.write(13, 0x000000001402A4CC);
         g_cpu().gpr.write(14, 0x000000002DE108EA);
         g_cpu().gpr.write(15, 0x000000003103E121);
@@ -117,13 +117,13 @@ void pif_hle_execute() {
         g_cpu().gpr.write(21, 0x0000000000000000);
         g_cpu().gpr.write(23, 0x0000000000000000);
         g_cpu().gpr.write(24, 0x0000000000000000);
-        g_cpu().gpr.write(25, 0xFFFFFFFF9DEBB54Fll);
+        g_cpu().gpr.write(25, 0xFFFFFFFF9DEBB54F);
         g_cpu().gpr.write(26, 0x0000000000000000);
         g_cpu().gpr.write(27, 0x0000000000000000);
         g_cpu().gpr.write(28, 0x0000000000000000);
-        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0ll);
+        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0);
         g_cpu().gpr.write(30, 0x0000000000000000);
-        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550ll);
+        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550);
 
         g_cpu().hi = 0x000000003FC18657;
         g_cpu().lo = 0x000000003103E121;
@@ -131,7 +131,7 @@ void pif_hle_execute() {
         if (pal) {
             g_cpu().gpr.write(20, 0x0000000000000000);
             g_cpu().gpr.write(23, 0x0000000000000006);
-            g_cpu().gpr.write(31, 0xFFFFFFFFA4001554ll);
+            g_cpu().gpr.write(31, 0xFFFFFFFFA4001554);
         }
     } break;
     case CicType::CIC_NUS_6103_7103: {
@@ -140,15 +140,15 @@ void pif_hle_execute() {
         g_cpu().gpr.write(2, 0x0000000049A5EE96);
         g_cpu().gpr.write(3, 0x0000000049A5EE96);
         g_cpu().gpr.write(4, 0x000000000000EE96);
-        g_cpu().gpr.write(5, 0xFFFFFFFFD4646273ll);
-        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0Cll);
-        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08ll);
+        g_cpu().gpr.write(5, 0xFFFFFFFFD4646273);
+        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0C);
+        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08);
         g_cpu().gpr.write(8, 0x00000000000000C0);
         g_cpu().gpr.write(9, 0x0000000000000000);
         g_cpu().gpr.write(10, 0x0000000000000040);
-        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040ll);
-        g_cpu().gpr.write(12, 0xFFFFFFFFCE9DFBF7ll);
-        g_cpu().gpr.write(13, 0xFFFFFFFFCE9DFBF7ll);
+        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040);
+        g_cpu().gpr.write(12, 0xFFFFFFFFCE9DFBF7);
+        g_cpu().gpr.write(13, 0xFFFFFFFFCE9DFBF7);
         g_cpu().gpr.write(14, 0x000000001AF99984);
         g_cpu().gpr.write(15, 0x0000000018B63D28);
         g_cpu().gpr.write(16, 0x0000000000000000);
@@ -159,18 +159,18 @@ void pif_hle_execute() {
         g_cpu().gpr.write(21, 0x0000000000000000);
         g_cpu().gpr.write(23, 0x0000000000000000);
         g_cpu().gpr.write(24, 0x0000000000000000);
-        g_cpu().gpr.write(25, 0xFFFFFFFF825B21C9ll);
+        g_cpu().gpr.write(25, 0xFFFFFFFF825B21C9);
         g_cpu().gpr.write(26, 0x0000000000000000);
         g_cpu().gpr.write(27, 0x0000000000000000);
         g_cpu().gpr.write(28, 0x0000000000000000);
-        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0ll);
+        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0);
         g_cpu().gpr.write(30, 0x0000000000000000);
-        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550ll);
+        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550);
 
         if (pal) {
             g_cpu().gpr.write(20, 0x0000000000000000);
             g_cpu().gpr.write(23, 0x0000000000000006);
-            g_cpu().gpr.write(31, 0xFFFFFFFFA4001554ll);
+            g_cpu().gpr.write(31, 0xFFFFFFFFA4001554);
         }
 
         g_cpu().lo = 0x0000000018B63D28;
@@ -179,17 +179,17 @@ void pif_hle_execute() {
     case CicType::CIC_NUS_6105_7105: {
         g_cpu().gpr.write(0, 0x0000000000000000);
         g_cpu().gpr.write(1, 0x0000000000000000);
-        g_cpu().gpr.write(2, 0xFFFFFFFFF58B0FBFll);
-        g_cpu().gpr.write(3, 0xFFFFFFFFF58B0FBFll);
+        g_cpu().gpr.write(2, 0xFFFFFFFFF58B0FBF);
+        g_cpu().gpr.write(3, 0xFFFFFFFFF58B0FBF);
         g_cpu().gpr.write(4, 0x0000000000000FBF);
-        g_cpu().gpr.write(5, 0xFFFFFFFFDECAAAD1ll);
-        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0Cll);
-        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08ll);
+        g_cpu().gpr.write(5, 0xFFFFFFFFDECAAAD1);
+        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0C);
+        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08);
         g_cpu().gpr.write(8, 0x00000000000000C0);
         g_cpu().gpr.write(9, 0x0000000000000000);
         g_cpu().gpr.write(10, 0x0000000000000040);
-        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040ll);
-        g_cpu().gpr.write(12, 0xFFFFFFFF9651F81Ell);
+        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040);
+        g_cpu().gpr.write(12, 0xFFFFFFFF9651F81E);
         g_cpu().gpr.write(13, 0x000000002D42AAC5);
         g_cpu().gpr.write(14, 0x00000000489B52CF);
         g_cpu().gpr.write(15, 0x0000000056584D60);
@@ -201,13 +201,13 @@ void pif_hle_execute() {
         g_cpu().gpr.write(21, 0x0000000000000000);
         g_cpu().gpr.write(23, 0x0000000000000000);
         g_cpu().gpr.write(24, 0x0000000000000002);
-        g_cpu().gpr.write(25, 0xFFFFFFFFCDCE565Fll);
+        g_cpu().gpr.write(25, 0xFFFFFFFFCDCE565F);
         g_cpu().gpr.write(26, 0x0000000000000000);
         g_cpu().gpr.write(27, 0x0000000000000000);
         g_cpu().gpr.write(28, 0x0000000000000000);
-        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0ll);
+        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0);
         g_cpu().gpr.write(30, 0x0000000000000000);
-        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550ll);
+        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550);
 
         g_cpu().lo = 0x0000000056584D60;
         g_cpu().hi = 0x000000004BE35D1F;
@@ -215,7 +215,7 @@ void pif_hle_execute() {
         if (pal) {
             g_cpu().gpr.write(20, 0x0000000000000000);
             g_cpu().gpr.write(23, 0x0000000000000006);
-            g_cpu().gpr.write(31, 0xFFFFFFFFA4001554ll);
+            g_cpu().gpr.write(31, 0xFFFFFFFFA4001554);
         }
 
         write_paddr32(PHYS_SPIMEM_BASE + 0x00, 0x3C0DBFC0);
@@ -231,18 +231,18 @@ void pif_hle_execute() {
     case CicType::CIC_NUS_6106_7106: {
         g_cpu().gpr.write(0, 0x0000000000000000);
         g_cpu().gpr.write(1, 0x0000000000000000);
-        g_cpu().gpr.write(2, 0xFFFFFFFFA95930A4ll);
-        g_cpu().gpr.write(3, 0xFFFFFFFFA95930A4ll);
+        g_cpu().gpr.write(2, 0xFFFFFFFFA95930A4);
+        g_cpu().gpr.write(3, 0xFFFFFFFFA95930A4);
         g_cpu().gpr.write(4, 0x00000000000030A4);
-        g_cpu().gpr.write(5, 0xFFFFFFFFB04DC903ll);
-        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0Cll);
-        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08ll);
+        g_cpu().gpr.write(5, 0xFFFFFFFFB04DC903);
+        g_cpu().gpr.write(6, 0xFFFFFFFFA4001F0C);
+        g_cpu().gpr.write(7, 0xFFFFFFFFA4001F08);
         g_cpu().gpr.write(8, 0x00000000000000C0);
         g_cpu().gpr.write(9, 0x0000000000000000);
         g_cpu().gpr.write(10, 0x0000000000000040);
-        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040ll);
-        g_cpu().gpr.write(12, 0xFFFFFFFFBCB59510ll);
-        g_cpu().gpr.write(13, 0xFFFFFFFFBCB59510ll);
+        g_cpu().gpr.write(11, 0xFFFFFFFFA4000040);
+        g_cpu().gpr.write(12, 0xFFFFFFFFBCB59510);
+        g_cpu().gpr.write(13, 0xFFFFFFFFBCB59510);
         g_cpu().gpr.write(14, 0x000000000CF85C13);
         g_cpu().gpr.write(15, 0x000000007A3C07F4);
         g_cpu().gpr.write(16, 0x0000000000000000);
@@ -257,9 +257,9 @@ void pif_hle_execute() {
         g_cpu().gpr.write(26, 0x0000000000000000);
         g_cpu().gpr.write(27, 0x0000000000000000);
         g_cpu().gpr.write(28, 0x0000000000000000);
-        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0ll);
+        g_cpu().gpr.write(29, 0xFFFFFFFFA4001FF0);
         g_cpu().gpr.write(30, 0x0000000000000000);
-        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550ll);
+        g_cpu().gpr.write(31, 0xFFFFFFFFA4001550);
 
         g_cpu().lo = 0x000000007A3C07F4;
         g_cpu().hi = 0x0000000023953898;
@@ -267,7 +267,7 @@ void pif_hle_execute() {
         if (pal) {
             g_cpu().gpr.write(20, 0x0000000000000000);
             g_cpu().gpr.write(23, 0x0000000000000006);
-            g_cpu().gpr.write(31, 0xFFFFFFFFA4001554ll);
+            g_cpu().gpr.write(31, 0xFFFFFFFFA4001554);
         }
     } break;
     default: {
@@ -281,7 +281,7 @@ void pif_hle_execute() {
     g_cpu().set_pc64(0xA4000040);
 
     // CPUのCOP0レジスタの初期化
-    // https://github.com/Dillonb/n64/blob/6502f7d2f163c3f14da5bff8cd6d5ccc47143156/src/mem/pif.c#L305
+    // https://github.com/Dionb/n64/blob/6502f7d2f163c3f14da5bff8cd6d5ccc47143156/src/mem/pif.c#L305
     g_cpu().cop0.reg.random = 0x0000001F;
     g_cpu().cop0.reg.status.raw = 0x34000000;
     g_cpu().cop0.reg.prid = 0x00000B00;
@@ -292,7 +292,7 @@ void pif_hle_execute() {
 
     // ROMの最初0x1000バイトをSP DMEMにコピー
     //   i.e. 0xB0000000 から 0xA4000000 に0x1000バイトをコピー
-    // https://github.com/Dillonb/n64/blob/6502f7d2f163c3f14da5bff8cd6d5ccc47143156/src/mem/pif.c#L358
+    // https://github.com/Dionb/n64/blob/6502f7d2f163c3f14da5bff8cd6d5ccc47143156/src/mem/pif.c#L358
     memcpy(g_rsp().get_sp_dmem().data(), g_memory().rom.get_raw_data().data(),
            sizeof(uint8_t) * 0x1000);
 }
