d4f17e4cd4e6741cc517138ec23dd434ee344dd7
Raiki Tamura <tamaron1203@gmail.com>
2023-08-07 02:03:47 +0900

    Create Window with SDL

--- src/frontend/frontend.cpp
+++ src/frontend/frontend.cpp
@@ -1,1 +1,14 @@
-// TODO:
+#include "vulkan_headers.hpp"
+#include <volk.h>
+#include <wsi.hpp>
+
+namespace N64 {
+namespace Frontend {
+
+using namespace Vulkan;
+
+WSI *wsi;
+VkInstance instance{};
+
+} // namespace Frontend
+} // namespace N64
--- src/frontend/frontend.h
+++ src/frontend/frontend.h
@@ -1,13 +1,26 @@
 #pragma once
 
+#include "utils.h"
+#include "vulkan_headers.hpp"
 #include <SDL.h>
 #include <volk.h>
+#include <wsi.hpp>
 
 namespace N64 {
 namespace Frontend {
 
 constexpr int WINDOW_WIDTH = 1024;
 constexpr int WINDOW_HEIGHT = 768;
+
+extern Vulkan::WSI *wsi;
+extern VkInstance instance;
+
+class SDLWSIPlatform final : public Vulkan::WSIPlatform {
+  public:
+    SDLWSIPlatform() = default;
+
+    // TODO: add methods
+};
 
 class Frontend {
   private:
@@ -19,6 +32,7 @@ class Frontend {
     VkPipelineCache pipelineCache{};
     VkDescriptorPool descriptorPool{};
     VkAllocationCallbacks *allocator{};
+    uint32_t minImageCount;
 
   public:
     Frontend() {
@@ -34,6 +48,33 @@ class Frontend {
         } else {
             Utils::info("Vulkan initialized");
         }
+
+        /*
+        wsi = new Vulkan::WSI();
+        wsi->set_backbuffer_srgb(false);
+        // wsi->set_platform(new Vulkan::WSIPlatform());
+        wsi->set_present_mode(Vulkan::PresentMode::SyncToVBlank);
+
+        Vulkan::Context::SystemHandles handles;
+        if (!wsi->init_simple(1, handles)) {
+            Utils::critical("Failed to initialize WSI");
+            exit(-1);
+        }
+
+        instance = wsi->get_context().get_instance();
+        physicalDevice = wsi->get_device().get_physical_device();
+        device = wsi->get_device().get_device();
+        queueFamily = wsi->get_context()
+                          .get_queue_info()
+                          .family_indices[Vulkan::QUEUE_INDEX_GRAPHICS];
+        queue = wsi->get_context()
+                    .get_queue_info()
+                    .queues[Vulkan::QUEUE_INDEX_GRAPHICS];
+        pipelineCache = nullptr;
+        descriptorPool = nullptr;
+        allocator = nullptr;
+        minImageCount = 2;
+        */
     }
 
     ~Frontend() {
