46f45df5dc14ce2e19dc11ddc7e8c858e8c6202e
Raiki Tamura <tamaron1203@gmail.com>
2023-08-09 18:41:59 +0900

    Implement frontend

--- src/app/app.h
+++ src/app/app.h
@@ -1,39 +1,53 @@
 #pragma once
 
+#include "app/window.h"
 #include "n64_system/config.h"
 #include "n64_system/n64_system.h"
 #include "utils/utils.h"
 #include <SDL.h>
+// imgui
+#include <backends/imgui_impl_sdl2.h>
+#include <backends/imgui_impl_sdlrenderer2.h>
+#include <imgui.h>
 
 namespace N64 {
 namespace Frontend {
 
-constexpr int WINDOW_WIDTH = 1024;
-constexpr int WINDOW_HEIGHT = 768;
-
 class App {
   private:
     N64System::Config config;
-    SDL_Window *window{};
+    Window *window = nullptr;
 
   public:
     App(N64System::Config &config) : config(config) {
         if (config.test_mode)
             return;
-        SDL_Init(SDL_INIT_EVERYTHING);
-        window = SDL_CreateWindow("n64-emu", SDL_WINDOWPOS_CENTERED,
-                                  SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH,
-                                  WINDOW_HEIGHT,
-                                  SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE |
-                                      SDL_WINDOW_ALLOW_HIGHDPI);
+        if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+            Utils::critical("Failed to initialize SDL: %s", SDL_GetError());
+            exit(-1);
+        }
+        window = new Window();
         // TODO: vulkan, wsi, imgui, etc.
     }
 
     ~App() {
         if (config.test_mode)
             return;
-        SDL_DestroyWindow(window);
+        delete window;
         SDL_Quit();
+    }
+
+    void init_imgui() {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io{ImGui::GetIO()};
+
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+        ImGui_ImplSDL2_InitForSDLRenderer(window->get_native_sdl_window(),
+                                          window->get_native_sdl_renderer());
+        ImGui_ImplSDLRenderer2_Init(window->get_native_sdl_renderer());
     }
 
     void run() {
@@ -44,6 +58,7 @@ class App {
 
             SDL_Event event;
             while (SDL_PollEvent(&event)) {
+                ImGui_ImplSDL2_ProcessEvent(&event);
                 switch (event.type) {
                 case SDL_QUIT: {
                     Utils::info("Stopping N64 system");
@@ -53,6 +68,14 @@ class App {
                     break;
                 }
             }
+
+            // TODO: remove this
+            SDL_SetRenderDrawColor(window->get_native_sdl_renderer(),
+                                   // Gray clear color (rgba)
+                                   100, 100, 100, 255);
+            SDL_RenderClear(window->get_native_sdl_renderer());
+            SDL_RenderPresent(window->get_native_sdl_renderer());
+
             // TODO: update screen every 1/60 seconds
         }
     }
---
+++ src/app/window.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "utils/utils.h"
+#include <SDL.h>
+
+namespace N64 {
+namespace Frontend {
+
+const char *WINDOW_TITLE = "n64-emu (dev)";
+constexpr int WINDOW_WIDTH = 1024;
+constexpr int WINDOW_HEIGHT = 768;
+
+class Window {
+  private:
+    SDL_Window *window{nullptr};
+    SDL_Renderer *renderer{nullptr};
+
+  public:
+    Window() {
+        window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH,
+                                  WINDOW_HEIGHT,
+                                  SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE |
+                                      SDL_WINDOW_ALLOW_HIGHDPI);
+        renderer = SDL_CreateRenderer(
+            window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+        if (renderer == nullptr) {
+            Utils::critical("Failed to create SDL_Renderer");
+            exit(-1);
+        }
+    }
+    ~Window() {
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+    }
+
+    SDL_Window *get_native_sdl_window() const { return window; }
+
+    SDL_Renderer *get_native_sdl_renderer() const { return renderer; }
+};
+
+} // namespace Frontend
+} // namespace N64
