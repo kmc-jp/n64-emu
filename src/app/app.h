aa4928feec04b5f24698a0e5c75e2a8e9a57b180
Raiki Tamura <tamaron1203@gmail.com>
2023-08-14 15:36:44 +0900

    wip

--- src/app/app.h
+++ src/app/app.h
@@ -1,22 +1,25 @@
 #pragma once
 
-#include "app/window.h"
+#include "device.hpp"
 #include "n64_system/config.h"
 #include "n64_system/n64_system.h"
 #include "utils/utils.h"
+#include "vulkan_headers.hpp"
+#include "wsi.hpp"
 #include <SDL.h>
-// imgui
-#include <backends/imgui_impl_sdl2.h>
-#include <backends/imgui_impl_sdlrenderer2.h>
-#include <imgui.h>
+#include <SDL_vulkan.h>
 
 namespace N64 {
 namespace Frontend {
 
+const char *WINDOW_TITLE = "n64-emu (dev)";
+constexpr int WINDOW_WIDTH = 1024;
+constexpr int WINDOW_HEIGHT = WINDOW_WIDTH * 3 / 4;
+
 class App {
   private:
     N64System::Config config;
-    Window *window = nullptr;
+    SDL_Window *window;
 
   public:
     App(N64System::Config &config) : config(config) {
@@ -24,36 +27,27 @@ class App {
             Utils::critical("Failed to initialize SDL: %s", SDL_GetError());
             exit(-1);
         }
-        window = new Window();
-        init_imgui();
+        window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH,
+                                  WINDOW_HEIGHT,
+                                  SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+        if (window == nullptr) {
+            Utils::critical("Failed to open Window");
+            exit(-1);
+        }
+        if (!Vulkan::Context::init_loader(
+                (PFN_vkGetInstanceProcAddr)
+                    SDL_Vulkan_GetVkGetInstanceProcAddr())) {
+            Utils::critical("Failed to load Vulkan");
+            exit(-1);
+        }
         // TODO: vulkan, wsi, etc.
     }
 
     ~App() {
-        fini_imgui();
-        delete window;
+        SDL_DestroyWindow(window);
+        SDL_Vulkan_UnloadLibrary();
         SDL_Quit();
-    }
-
-    void init_imgui() {
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-        // ImGuiIO &io{ImGui::GetIO()};
-        // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-        //  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
-
-        // TODO: dockspace
-        // https://martin-fieber.de/blog/gui-development-with-cpp-sdl2-and-dear-imgui/
-
-        ImGui_ImplSDL2_InitForSDLRenderer(window->get_native_sdl_window(),
-                                          window->get_native_sdl_renderer());
-        ImGui_ImplSDLRenderer2_Init(window->get_native_sdl_renderer());
-    }
-
-    void fini_imgui() {
-        ImGui_ImplSDLRenderer2_Shutdown();
-        ImGui_ImplSDL2_Shutdown();
-        ImGui::DestroyContext();
     }
 
     void run() {
@@ -64,7 +58,6 @@ class App {
 
             SDL_Event event;
             while (SDL_PollEvent(&event)) {
-                ImGui_ImplSDL2_ProcessEvent(&event);
                 switch (event.type) {
                 case SDL_QUIT: {
                     Utils::info("Stopping application");
@@ -74,32 +67,6 @@ class App {
                     break;
                 }
             }
-
-            // Start imgui frame
-            ImGui_ImplSDLRenderer2_NewFrame();
-            ImGui_ImplSDL2_NewFrame();
-            ImGui::NewFrame();
-
-            // TODO: render some panel
-            ImGui::Begin("TODO: some panel");
-            ImGui::Text("Hi, I am some panel :-)");
-            ImGui::End();
-
-            // Rendering
-            ImGui::Render();
-
-            // TODO: remove from here ...
-            SDL_SetRenderDrawColor(window->get_native_sdl_renderer(),
-                                   // Gray clear color (rgba)
-                                   100, 100, 100, 255);
-            SDL_RenderClear(window->get_native_sdl_renderer());
-            // ... to here?
-
-            // Render data through the SDL renderer
-            ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
-            SDL_RenderPresent(window->get_native_sdl_renderer());
-
-            // TODO: update screen every 1/60 seconds
         }
     }
 };
