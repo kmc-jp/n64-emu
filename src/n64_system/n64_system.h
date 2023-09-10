5cbef108f1076adb8b94ec1cb366ea93c0867032
Raiki Tamura <tamaron1203@gmail.com>
2023-09-10 15:30:06 +0900

    Chore

--- src/n64_system/n64_system.h
+++ src/n64_system/n64_system.h
@@ -7,6 +7,16 @@
 namespace N64 {
 namespace N64System {
 
+// TODO: unused now. should remove?
+enum class N64Renderer {
+    PARALLEL_RDP,
+    CPU_RENDERER,
+};
+
+// TODO: unused now. should remove?
+// TODO: support other renderer
+constexpr N64Renderer n64_renderer = N64Renderer::CPU_RENDERER;
+
 void set_up(Config &config);
 
 void step(Config &config, Vulkan::WSI &wsi);
