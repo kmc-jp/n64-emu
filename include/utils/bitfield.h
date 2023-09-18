7df7b003c3c67de39f6bcecca16bcacc5f19aba7
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-09-18 18:08:24 +0900

    Update bitfield_t

--- include/utils/bitfield.h
+++ include/utils/bitfield.h
@@ -12,8 +12,6 @@ template <Constant T, Index I> struct ref_t {
   private:
     static constexpr std::size_t offset = I::value;
     static constexpr value_type mask = T::value << offset;
-    value_type unshift() const { return; }
-    value_type shift(value_type v) const { return (v << offset) & mask; }
 
   public:
     value_type *ptr;
@@ -27,17 +25,14 @@ template <Constant T, Index I> struct ref_t {
 
 template <std::unsigned_integral T> struct bitfield_t {
   private:
-    T *ptr;
+    static constexpr auto zero = constant_t<T, 0>{};
+    static constexpr auto size = index_t<(sizeof(T) * 8)>{};
+    template <std::size_t L>
+    using range_t = decltype((~zero) >> (size - index_t<L>{}));
 
   public:
-    bitfield_t(T &raw) : ptr(&raw) {}
-    template <std::size_t I, std::size_t L> auto field() -> decltype(auto) {
-        constexpr auto zero = constant_t<T, 0>{};
-        constexpr auto size = index_t<(sizeof(T) * 8)>{};
-        constexpr auto length = index_t<L>{};
-        using range_t = decltype((~zero) >> (size - size.min(length)));
-        return ref_t<range_t, index_t<I>>{*ptr};
-    }
+    template <std::size_t I, std::size_t L>
+    using field_t = ref_t<range_t<L>, index_t<I>>;
 };
 
 } // namespace Utils
--- src/tests/bitfield.cpp
+++ src/tests/bitfield.cpp
@@ -13,48 +13,49 @@ static_assert(Index<constant_t<std::size_t, 5>>);
 namespace selftest {
 
 struct bf_t : Utils::bitfield_t<std::uint32_t> {
-    bf_t(std::uint32_t &raw) : bitfield_t(raw) {}
-    auto a() -> decltype(auto) { return field<0, 1>(); }
-    auto b() -> decltype(auto) { return field<1, 2>(); }
-    auto c() -> decltype(auto) { return field<3, 3>(); }
-    auto d() -> decltype(auto) { return field<6, 4>(); }
-    auto e() -> decltype(auto) { return field<10, 5>(); }
-    auto lo() -> decltype(auto) { return field<0, 16>(); }
-    auto hi() -> decltype(auto) { return field<16, 16>(); }
+    bf_t(std::uint32_t &raw)
+        : a{raw}, b{raw}, c{raw}, d{raw}, e{raw}, lo{raw}, hi{raw} {}
+    field_t<0, 1> a;
+    field_t<1, 2> b;
+    field_t<3, 3> c;
+    field_t<6, 4> d;
+    field_t<10, 5> e;
+    field_t<0, 16> lo;
+    field_t<16, 16> hi;
 };
 
 void bitfield_test() {
     std::uint32_t raw = 0, expect = 0;
     bf_t bf{raw};
     test_eq(expect, raw);
-    bf.a() = 0b1;
+    bf.a = 0b1;
     expect |= 0b1;
     test_eq(expect, raw);
-    bf.b() = 3;
+    bf.b = 3;
     expect |= 0b110;
     test_eq(expect, raw);
-    bf.c() = 0b111;
+    bf.c = 0b111;
     expect |= 0b111000;
     test_eq(expect, raw);
-    bf.d() = 0b1111;
+    bf.d = 0b1111;
     expect |= 0b1111000000;
     test_eq(expect, raw);
-    bf.e() = 0b11111;
+    bf.e = 0b11111;
     expect |= 0b111110000000000;
     test_eq(expect, raw);
     test_eq(0x7fff, raw);
-    bf.hi() = 0x4321;
+    bf.hi = 0x4321;
     test_eq(0x4321'7fff, raw);
-    bf.lo() = 0x8765;
+    bf.lo = 0x8765;
     test_eq(0x4321'8765, raw);
     test_eq(0b0100'0011'0010'0001'1'00001'1101'100'10'1, raw);
-    test_eq(0b1, *bf.a());
-    test_eq(0b10, *bf.b());
-    test_eq(0b100, *bf.c());
-    test_eq(0b1101, *bf.d());
-    test_eq(0b00001, *bf.e());
-    test_eq(0x8765, *bf.lo());
-    test_eq(0x4321, *bf.hi());
+    test_eq(0b1, *bf.a);
+    test_eq(0b10, *bf.b);
+    test_eq(0b100, *bf.c);
+    test_eq(0b1101, *bf.d);
+    test_eq(0b00001, *bf.e);
+    test_eq(0x8765, *bf.lo);
+    test_eq(0x4321, *bf.hi);
 }
 
 } // namespace selftest
