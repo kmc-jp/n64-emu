2389b057f4125dc799f3c860f389ce3fe4e65160
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-09-20 22:11:59 +0900

    Update bitfield test

--- src/tests/bitfield.cpp
+++ src/tests/bitfield.cpp
@@ -9,74 +9,78 @@ static_assert(Constant<index_t<3>>);
 static_assert(Index<index_t<4>>);
 static_assert(Index<constant_t<std::size_t, 5>>);
 } // namespace Utils
+namespace {
+void test1() {
+    using selftest::test_eq;
+    struct bf_t : Utils::bitfield_t<std::uint32_t> {
+        bf_t(std::uint32_t &raw)
+            : a{raw}, b{raw}, c{raw}, d{raw}, e{raw}, lo{raw}, hi{raw} {}
+        field_t<0, 1> a;
+        field_t<1, 2> b;
+        field_t<3, 3> c;
+        field_t<6, 4> d;
+        field_t<10, 5> e;
+        field_t<0, 16> lo;
+        field_t<16, 16> hi;
+    };
+    std::uint32_t raw = 0, expect = 0;
+    bf_t bf{raw};
+    test_eq(expect, raw);
+    bf.a = 0b1;
+    expect |= 0b1;
+    test_eq(expect, raw);
+    bf.b = 3;
+    expect |= 0b110;
+    test_eq(expect, raw);
+    bf.c = 0b111;
+    expect |= 0b111000;
+    test_eq(expect, raw);
+    bf.d = 0b1111;
+    expect |= 0b1111000000;
+    test_eq(expect, raw);
+    bf.e = 0b11111;
+    expect |= 0b111110000000000;
+    test_eq(expect, raw);
+    test_eq(0x7fff, raw);
+    bf.hi = 0x4321;
+    test_eq(0x4321'7fff, raw);
+    bf.lo = 0x8765;
+    test_eq(0x4321'8765, raw);
+    test_eq(0b0100'0011'0010'0001'1'00001'1101'100'10'1, raw);
+    test_eq(0b1, *bf.a);
+    test_eq(0b10, *bf.b);
+    test_eq(0b100, *bf.c);
+    test_eq(0b1101, *bf.d);
+    test_eq(0b00001, *bf.e);
+    test_eq(0x8765, *bf.lo);
+    test_eq(0x4321, *bf.hi);
+}
+void test2() {
+    using selftest::test_eq;
+    struct example_t : Utils::bitfield_t<std::uint8_t> {
+        example_t(std::uint8_t &raw) : a{raw}, b{raw}, c{raw} {}
+        field_t<0, 2> a; // 2 bits at offset 0
+        field_t<2, 3> b; // 3 bits at offset 2
+        field_t<6, 1> c; // 1 bits at offset 6
+    };
+    std::uint8_t raw = 0;
+    example_t ex{raw};
+    ex.a = 0b11;
+    test_eq(0b00000011, raw);
+    ex.b = 0b110;
+    test_eq(0b00011011, raw);
+    ex.c = 0b1;
+    test_eq(0b01011011, raw);
+    raw = 0b01010101;
+    test_eq(0b01, *ex.a);
+    test_eq(0b101, *ex.b);
+    test_eq(0b1, *ex.c);
+}
+} // namespace
 
 namespace selftest {
-
-struct bf_t : Utils::bitfield_t<std::uint32_t> {
-    bf_t(std::uint32_t &raw)
-        : a{raw}, b{raw}, c{raw}, d{raw}, e{raw}, lo{raw}, hi{raw} {}
-    field_t<0, 1> a;
-    field_t<1, 2> b;
-    field_t<3, 3> c;
-    field_t<6, 4> d;
-    field_t<10, 5> e;
-    field_t<0, 16> lo;
-    field_t<16, 16> hi;
-};
-struct example_t : Utils::bitfield_t<std::uint8_t> {
-    example_t(std::uint8_t &raw) : a{raw}, b{raw}, c{raw} {}
-    field_t<0, 2> a; // 2 bits at offset 0
-    field_t<2, 3> b; // 3 bits at offset 2
-    field_t<6, 1> c; // 1 bits at offset 6
-};
 void bitfield_test() {
-    {
-        std::uint32_t raw = 0, expect = 0;
-        bf_t bf{raw};
-        test_eq(expect, raw);
-        bf.a = 0b1;
-        expect |= 0b1;
-        test_eq(expect, raw);
-        bf.b = 3;
-        expect |= 0b110;
-        test_eq(expect, raw);
-        bf.c = 0b111;
-        expect |= 0b111000;
-        test_eq(expect, raw);
-        bf.d = 0b1111;
-        expect |= 0b1111000000;
-        test_eq(expect, raw);
-        bf.e = 0b11111;
-        expect |= 0b111110000000000;
-        test_eq(expect, raw);
-        test_eq(0x7fff, raw);
-        bf.hi = 0x4321;
-        test_eq(0x4321'7fff, raw);
-        bf.lo = 0x8765;
-        test_eq(0x4321'8765, raw);
-        test_eq(0b0100'0011'0010'0001'1'00001'1101'100'10'1, raw);
-        test_eq(0b1, *bf.a);
-        test_eq(0b10, *bf.b);
-        test_eq(0b100, *bf.c);
-        test_eq(0b1101, *bf.d);
-        test_eq(0b00001, *bf.e);
-        test_eq(0x8765, *bf.lo);
-        test_eq(0x4321, *bf.hi);
-    }
-    {
-        std::uint8_t raw = 0;
-        example_t ex{raw};
-        ex.a = 0b11;
-        test_eq(0b00000011, raw);
-        ex.b = 0b110;
-        test_eq(0b00011011, raw);
-        ex.c = 0b1;
-        test_eq(0b01011011, raw);
-        raw = 0b01010101;
-        test_eq(0b01, *ex.a);
-        test_eq(0b101, *ex.b);
-        test_eq(0b1, *ex.c);
-    }
+    test1();
+    test2();
 }
-
 } // namespace selftest
