2147c0bd74e128e55206500b092a3c4da88914d3
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-09-20 22:11:47 +0900

    Fix constant_t

--- include/utils/constant.h
+++ include/utils/constant.h
@@ -13,8 +13,8 @@ struct is_constant<constant_t<T, V>> : std::true_type {};
 template <typename T>
 concept Constant = is_constant<T>::value;
 template <typename T>
-concept Index = is_constant<T>::value &&
-                std::is_same_v<typename T::value_type, std::size_t>;
+concept Index =
+    Constant<T> && std::is_same_v<typename T::value_type, std::size_t>;
 
 template <std::unsigned_integral T, T V>
 struct constant_t : std::integral_constant<T, V> {
@@ -22,59 +22,50 @@ struct constant_t : std::integral_constant<T, V> {
 
   private:
     template <value_type U> using self = constant_t<value_type, U>;
-    static constexpr std::size_t size = sizeof(value_type) * 8;
-    static constexpr value_type zero = 0, one = 1;
-    static constexpr value_type full = ~zero, half = one << (size - 1);
+    template <typename U> static consteval value_type cast(U v) {
+        return static_cast<value_type>(v);
+    }
+    static constexpr value_type lhs = V;
 
   public:
     template <value_type W>
-    consteval auto operator+(self<W>) const -> decltype(auto) {
-        if constexpr (V <= full - W) {
-            return self<(V + W)>{};
-        } else if constexpr (V >= half && W >= half) {
-            return self<((V - half) + (W - half))>{};
-        } else if constexpr (V < W) {
-            return self<((V + (W - half)) - half)>{};
-        } else {
-            return self<(((V - half) + W) - half)>{};
-        }
+    consteval auto operator+(self<W> rhs) const -> decltype(auto) {
+        return self<cast(lhs + rhs)>{};
     }
     template <value_type W>
-    consteval auto operator-(self<W>) const -> decltype(auto) {
-        if constexpr (V < W) {
-            return self<(V + (full - W) + one)>{};
-        } else {
-            return self<(V - W)>{};
-        }
+    consteval auto operator-(self<W> rhs) const -> decltype(auto) {
+        return self<cast(lhs - rhs)>{};
     }
     template <value_type W>
-    consteval auto operator&(self<W>) const -> decltype(auto) {
-        return self<(V & W)>{};
+    consteval auto operator&(self<W> rhs) const -> decltype(auto) {
+        return self<cast(lhs & rhs)>{};
     }
     template <value_type W>
-    consteval auto operator|(self<W>) const -> decltype(auto) {
-        return self<(V | W)>{};
+    consteval auto operator|(self<W> rhs) const -> decltype(auto) {
+        return self<cast(lhs | rhs)>{};
     }
     template <value_type W>
-    consteval auto operator^(self<W>) const -> decltype(auto) {
-        return self<(V ^ W)>{};
+    consteval auto operator^(self<W> rhs) const -> decltype(auto) {
+        return self<cast(lhs ^ rhs)>{};
     }
     consteval auto operator~() const -> decltype(auto) {
-        return self<static_cast<value_type>(~V)>{};
+        return self<cast(~lhs)>{};
     }
-    template <Index I> constexpr auto operator<<(I) const -> decltype(auto) {
-        return self<((V & (full >> I::value)) << I::value)>{};
+    template <Index I>
+    consteval auto operator<<(I rhs) const -> decltype(auto) {
+        return self<cast(lhs << rhs)>{};
     }
-    template <Index I> constexpr auto operator>>(I) const -> decltype(auto) {
-        return self<(V >> I::value)>{};
-    }
-    template <value_type W>
-    consteval auto min(self<W>) const -> decltype(auto) {
-        return self<((V < W) ? V : W)>{};
+    template <Index I>
+    consteval auto operator>>(I rhs) const -> decltype(auto) {
+        return self<cast(lhs >> rhs)>{};
     }
     template <value_type W>
-    consteval auto max(self<W>) const -> decltype(auto) {
-        return self<((V < W) ? W : V)>{};
+    consteval auto min(self<W> rhs) const -> decltype(auto) {
+        return self<((lhs < rhs) ? lhs : rhs)>{};
+    }
+    template <value_type W>
+    consteval auto max(self<W> rhs) const -> decltype(auto) {
+        return self<((lhs < rhs) ? rhs : lhs)>{};
     }
 };
 template <std::size_t I> using index_t = constant_t<std::size_t, I>;
