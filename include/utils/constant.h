f32913b4aac75d01c4fd230813a7936543cf1633
hatsusato <2765630+hatsusato@users.noreply.github.com>
2023-09-30 12:57:50 +0900

    Fix concepts

--- include/utils/constant.h
+++ include/utils/constant.h
@@ -2,40 +2,40 @@
 #define INCLUDE_GUARD_21FEA685_23A5_43ED_A210_5762B13C987F
 
 #include <concepts>
-#include <cstdint>
+#include <cstddef>
 #include <type_traits>
 
 namespace Utils {
 namespace detail {
-template <std::size_t N> struct Uint;
-template <> struct Uint<8> : std::type_identity<std::uint8_t> {};
-template <> struct Uint<16> : std::type_identity<std::uint16_t> {};
-template <> struct Uint<32> : std::type_identity<std::uint32_t> {};
-template <> struct Uint<64> : std::type_identity<std::uint64_t> {};
+template <typename T>
+concept StdIntegralConstant = requires(T t) {
+    // static constexpr V value = v;
+    { T::value } -> std::convertible_to<typename T::value_type>;
+    // using value_type = V;
+    typename T::value_type;
+    // using type       = integral_constant<V,v>;
+    std::is_same_v<T, std::integral_constant<typename T::value_type, T::value>>;
+    // constexpr operator value_type() noexcept;
+    {
+        t.operator typename T::value_type()
+    } noexcept -> std::same_as<typename T::value_type>;
+    // constexpr value_type operator()() const noexcept;
+    { t.operator()() } noexcept -> std::same_as<typename T::value_type>;
+};
+
+template <typename T> struct IsUint128 : std::false_type {};
 #ifdef __SIZEOF_INT128__
-#define UINT128_T_DEFINED
-template <> struct Uint<128> : std::type_identity<unsigned __int128> {};
+template <> struct IsUint128<unsigned __int128> : std::true_type {};
 #endif
 } // namespace detail
-template <std::size_t N> using Uint = typename detail::Uint<N>::type;
+
+template <typename T>
+concept Index = detail::StdIntegralConstant<T> &&
+                std::is_same_v<std::size_t, typename T::value_type>;
 
 template <typename T>
 concept UnsignedIntegral =
-    std::unsigned_integral<T> ||
-    std::same_as<std::remove_cv_t<T>, Uint<(sizeof(T) * 8)>>;
-template <UnsignedIntegral T>
-using BitsOf = std::integral_constant<std::size_t, sizeof(T) * 8>;
-
-template <UnsignedIntegral T, T V> struct IntegralConstant;
-template <typename T> struct IsConstant : std::false_type {};
-template <UnsignedIntegral T, T V>
-struct IsConstant<IntegralConstant<T, V>> : std::true_type {};
-
-template <typename T>
-concept Constant = IsConstant<T>::value;
-template <typename T>
-concept Index =
-    Constant<T> && std::is_same_v<typename T::value_type, std::size_t>;
+    std::unsigned_integral<T> || detail::IsUint128<std::remove_cv_t<T>>::value;
 
 template <UnsignedIntegral T, T V>
 struct IntegralConstant : std::integral_constant<T, V> {
@@ -90,14 +90,24 @@ struct IntegralConstant : std::integral_constant<T, V> {
     }
 };
 
+namespace detail {
+template <typename> struct IsConstant : std::false_type {};
+template <typename T, T V>
+struct IsConstant<IntegralConstant<T, V>> : std::true_type {};
+} // namespace detail
+template <typename T>
+concept Constant = detail::IsConstant<std::remove_cv_t<T>>::value;
+
 template <std::size_t I>
 constexpr auto index_const = IntegralConstant<std::size_t, I>{};
+template <typename T> constexpr auto bits_of = index_const<sizeof(T) * 8>;
+
 template <UnsignedIntegral T>
 constexpr auto zero_const = IntegralConstant<T, 0>{};
-template <UnsignedIntegral T, std::size_t W = BitsOf<T>{}, std::size_t I = 0,
-          std::enable_if_t<(W + I <= BitsOf<T>{}), std::nullptr_t> = nullptr>
+template <UnsignedIntegral T, std::size_t W = bits_of<T>, std::size_t I = 0>
+    requires(W + I <= bits_of<T>)
 constexpr auto mask_const =
-    ((~zero_const<T>) >> (index_const<BitsOf<T>{} - W>)) << (index_const<I>);
+    ((~zero_const<T>) >> (index_const<bits_of<T> - W>)) << (index_const<I>);
 } // namespace Utils
 
 #endif // INCLUDE_GUARD_21FEA685_23A5_43ED_A210_5762B13C987F
